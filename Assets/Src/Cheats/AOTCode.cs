dddb88d75fef3b2778e9fda36fb03f729f5c9546
Miguel Tomas <miguel@firstlight.games>
2020-09-25 10:25:30 +0100

    Added AOT code for deserialized data

---
+++ Assets/Src/Cheats/AOTCode.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using FirstLight.Game.Data;
+
+namespace Cheats
+{
+	/// <summary>
+	/// This is a helper class for AOT compilation code.
+	/// IMPORTANT: Do not reference or use any of the code in this object
+	/// </summary>
+	public class AOTCode
+	{
+		public List<AppData> AppData;
+		public List<LocalData> LocalData;
+		public List<PlayerData> PlayerData;
+	}
+}
